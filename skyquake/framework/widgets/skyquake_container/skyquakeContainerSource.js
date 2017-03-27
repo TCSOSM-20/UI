8ab41d9e9bd54549a0d9eda1255b570b22ca9ca8
KIRAN KASHALKAR <kiran.kashalkar@riftio.com>
2017-03-27 16:38:05 -0400

    Bug 209
    Signed-off-by: KIRAN KASHALKAR <kiran.kashalkar@riftio.com>

--- skyquake/framework/widgets/skyquake_container/skyquakeContainerSource.js
+++ skyquake/framework/widgets/skyquake_container/skyquakeContainerSource.js
@@ -79,7 +79,7 @@ export default {
             remote: function(state, location, streamSource) {
                 return new Promise((resolve, reject) => {
                     $.ajax({
-                        url: '//' + window.location.hostname + ':' + window.location.port + '/socket-polling',
+                        url: '//' + window.location.hostname + ':' + window.location.port + '/socket-polling?api_server=' + API_SERVER,
                         type: 'POST',
                         beforeSend: Utils.addAuthorizationStub,
                         data: {
